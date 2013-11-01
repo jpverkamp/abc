07cabda840c47822a1147e559562903182e84440
JP Verkamp <me@jverkamp.com>
2013-11-01 14:28:44 -0700

    Replaced lexer tokens with my own structs so that match works

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -12,44 +12,44 @@
    ; Header lines / inline headers
    [(:or (:: (:/ "AZ") #\: (:* whitespace) (:* (:~ #\newline)) (:* whitespace) (:? #\newline))
          (:: #\[ (:/ "AZ") #\: (:* whitespace) (:* (:~ #\])) (:* whitespace) #\]))
-    (token-header lexeme)]
+    (header lexeme)]
    
    ; Pitches
    [(:: (:? (:or "^^" "^" "__" "_" "="))
         (:/ "agAG")
         (:* (:or "'" ",")))
-    (token-pitch lexeme)]
+    (pitch lexeme)]
    
    ; Timing information
    [(:or (:: (:+ numeric) (:+ #\/) (:+ numeric))
          (:: (:+ #\/) (:+ numeric))
          (:+ numeric)
          (:+ #\/))
-    (token-duration lexeme)]
+    (duration lexeme)]
    
    ; Bar lines
-   ["||" (token-double-bar)]
-   ["[|" (token-double-bar-start)]
-   ["|]" (token-double-bar-end)]
-   ["|:" (token-repeat-start)]
-   [":|" (token-repeat-end)]
-   [(:or ":|:" "::") (token-repeat-end-start)]
-   ["|"  (token-bar)]
+   ["||" (double-bar)]
+   ["[|" (double-bar-start)]
+   ["|]" (double-bar-end)]
+   ["|:" (repeat-start)]
+   [":|" (repeat-end)]
+   [(:or ":|:" "::") (repeat-end-start)]
+   ["|"  (bar)]
    
    ; Simple tokens
-   [#\- (token-tie)]
-   [#\[ (token-chord-start)] [#\] (token-chord-end)]
-   [#\( (token-slur-start)]  [#\) (token-slur-end)]
-   [#\{ (token-grace-start)] [#\} (token-grace-end)]
-   [#\z (token-rest)]        [#\Z (token-long-rest)]
+   [#\- (tie)]
+   [#\[ (chord-start)] [#\] (chord-end)]
+   [#\( (slur-start)]  [#\) (slur-end)]
+   [#\{ (grace-start)] [#\} (grace-end)]
+   [#\z (rest)]        [#\Z (long-rest)]
    
    ; Text / guitar chords
    [(:: #\" (:* (:or (:: #\\ any-char) (:~ #\"))) #\")
-    (token-text (string-trim lexeme "\""))]
+    (text (string-trim lexeme "\""))]
    
    ; Line comments
    [(:: #\% (:* (:~ #\newline)))
-    (token-comment lexeme)]
+    (comment lexeme)]
    
    ; Whitespace breaks notes sharing a bar (\ continues lines, so treat as a normal break)
    [(:or (:: #\\ (:* whitespace))
@@ -57,7 +57,7 @@
     (abc-lexer input-port)]
    
    ; Newlines break lines in output as well (unless escaped with \, see above)
-   [#\newline (token-linebreak)]))
+   [#\newline (linebreak)]))
 
 ; Lex a document into a list given a lexer
 (define (lex lexer in)
--- tokens.rkt
+++ tokens.rkt
@@ -1,20 +1,28 @@
 #lang racket
 
-(require 
- parser-tools/lex)
+(define-syntax-rule (define-and-provide-tokens name ...)
+  (begin
+    (begin
+      (provide (struct-out name))
+      (struct name (value) #:transparent))
+    ...))
 
-(provide 
- (all-defined-out))
+(define-and-provide-tokens 
+  header pitch duration text comment ending)
 
-(define-tokens abc 
-  (header pitch duration text comment ending))
+(define-syntax-rule (define-and-provide-empty-tokens name ...)
+  (begin
+    (begin
+      (provide name)
+      (define (name) 'name))
+    ...))
 
-(define-empty-tokens abc-empty
-  (tie
-   chord-start chord-end
-   slur-start slur-end
-   grace-start grace-end
-   rest long-rest 
-   bar double-bar double-bar-start double-bar-end 
-   repeat-start repeat-end repeat-end-start
-   break linebreak))
+(define-and-provide-empty-tokens 
+  tie
+  chord-start chord-end
+  slur-start slur-end
+  grace-start grace-end
+  rest long-rest 
+  bar double-bar double-bar-start double-bar-end 
+  repeat-start repeat-end repeat-end-start
+  break linebreak)
